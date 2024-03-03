c5d4b294b86ec47e66e628378823a494369a0f14
David O'Connell <dvdgeroconnell@gmail.com>
2024-03-03 15:17:46 +0000

    Create test-code folder, tidy up repo

---
+++ test-code/dice.py
@@ -0,0 +1,23 @@
+# Week 5 - test various functions associated with numpy
+# Author: David O'Connell
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+rolls = np.random.randint(1, 7, 1000)
+print(rolls[:25])
+mean = np.mean(rolls)
+print(mean)
+std = np.std(rolls)
+print(std)
+
+# in the following code, 2 arrays are returned. The first goes into 
+# unique_rolls, the second goes into roll_counts.
+unique_rolls, roll_counts = np.unique(rolls, return_counts=True)
+print(unique_rolls)
+print(roll_counts)
+
+plt.bar(unique_rolls, roll_counts)
+
+# This function prints the bar chart in its own window.
+plt.show()
